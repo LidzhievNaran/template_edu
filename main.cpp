e23bf7bf560d477373406f8cb8de4b370528a940
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-03-21 10:55:23 +0300

    вывод типа шаблона исходя из параметров функции

--- main.cpp
+++ main.cpp
@@ -6,16 +6,32 @@ void my_swap( T & first, T & second)
     first = second;
     second = temp;
 }
+template < class ElementType>
+void bubbleSort(ElementType * arr, size_t arrSize)
+{
+    for (size_t i =0; i < arrSize - 1; ++i)
+        for (size_t j =0; j < arrSize - 1; ++j)
+            if (arr[j+1] < arr[j])
+                my_swap(arr[j], arr[j+1]);
+}
+
+template <typename ElementType>
+void out_array (const ElementType * arr, size_t arrSize)
+{
+    for (size_t i =0; i < arrSize; ++i)
+        std::cout << arr[i]<< ' ';
+    std::cout << std::endl;
+}
 int main() {
-    int a = 5;
-    int b = 10;
-    std::cout << a << " " << b << std::endl;
-    my_swap<int>(a , b);
-    std::cout << a << " " << b << std::endl;
-    double c = 77.89;
-    double d = 54.22 ;
-    std::cout << c << " " << d << std::endl;
-    my_swap<double>(c , d);
-    std::cout << c << " " << d << std::endl;
-    return 0;
+  const size_t n = 5;
+  int arr1[ n ] = {10, 5, 7 ,3 , 4};
+  double arr2 [n] = {7.62, 5.56, 38.0 , 56.0, 9.0};
+  std::cout << "Source arrays:\n";
+    out_array(arr1, n);
+    out_array(arr2, n);
+    bubbleSort(arr1, n);
+    bubbleSort(arr2, n);
+    std::cout << "Sorted arrays:\n";
+    out_array(arr1, n);
+    out_array(arr2, n);
 }
